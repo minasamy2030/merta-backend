b6f2ef261042b81b35d6420f0e3102ecde421385
Mina <dr.minasamy@gmail.com>
2025-11-17 14:32:05 +0200

    setup cloudinary

--- merta-admin/config/middlewares.ts
+++ merta-admin/config/middlewares.ts
@@ -1,7 +1,21 @@
 export default [
   'strapi::logger',
   'strapi::errors',
-  'strapi::security',
+  // دور على الجزء ده وعدله عشان يبقى كده:
+{
+  name: 'strapi::security',
+  config: {
+    contentSecurityPolicy: {
+      useDefaults: true,
+      directives: {
+        'connect-src': ["'self'", 'https:'],
+        'img-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+        'media-src': ["'self'", 'data:', 'blob:', 'market-assets.strapi.io', 'res.cloudinary.com'],
+        upgradeInsecureRequests: null,
+      },
+    },
+  },
+},
   'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
